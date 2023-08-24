661d6bab8a9ace7508655dfc747fd7ecdd2c0392
Asura <jomydkizhakeyil@gmail.com>
2023-08-24 18:39:08 +0530

    Admin Dashboard Added

--- gallery_app/urls.py
+++ gallery_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     path('',views.home),
+    path('administrator',views.admin)
     
 ]
--- gallery_app/views.py
+++ gallery_app/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'main/index.html')
+
+def admin(request):
+    return render(request,'administrator/index.html')
