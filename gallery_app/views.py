0d54c59aa61a0818dbf22ba22138068336d44839
Asura <jomydkizhakeyil@gmail.com>
2023-09-01 01:31:25 +0530

    80% done with basic Layout

--- gallery_app/views.py
+++ gallery_app/views.py
@@ -10,11 +10,34 @@ from random import randint
 
 def home(request):
     context = {}
+    sql = "select * from category_list"
+    context['cat']=connections.selectall(sql)
     if 'admin' in request.session:
         context['admin_data']=request.session['admin']
         return render(request,'main/index.html',context)
+    elif 'artist' in request.session:
+        context['user_data']=request.session['artist']
+        return render(request,'main/index.html',context)
     return render(request,'main/index.html')
 
+def Gallery(request):
+    context = {}
+    if request.GET:
+        id = request.GET['id']
+        sql = "select * from category_list"
+        context['cat']=connections.selectall(sql)
+        sql = "select * from category_list where cat_id='"+id+"'"
+        cat_details = connections.select(sql)
+    
+        if 'admin' in request.session:
+            context['admin_data']=request.session['admin']
+            return render(request,'main/gallery.html',context)
+        elif 'artist' in request.session:
+            context['user_data']=request.session['artist']
+            return render(request,'main/gallery.html',context)
+        else:
+            return render(request,'main/gallery.html',context)
+    return HttpResponseRedirect('error')
 
 # Registration and login
 
@@ -40,11 +63,13 @@ def register(request):
                 connections.insert(sql)
                 return render(request,'main/login.html',{'msg':"Registration Successful"})
             else:
-                pass
+                return render(request,'main/login.html',{'msg':"Registration Successful"})
     return render(request,'main/register.html')
 
 def login(request):
     if 'admin' in request.session.keys():
+        return HttpResponseRedirect('home')
+    elif 'artist' in request.session.keys():
         return HttpResponseRedirect('home')
     else:
         context={}
@@ -55,12 +80,18 @@ def login(request):
             user = connections.select(sql)
             if user:
                 if user[2]=='artist' and user[3]==1:
-                    request.session['artist']=user
-                    context['user_data']=user
+                    sql = "select * from artist_list where artist_id = '"+user_name+"'"
+                    user_data = connections.select(sql)
+                    request.session['artist']=user_data
+                    context['user_data']=user_data
                     return render(request,'main/index.html',context)
                 elif user[2]=='admin':
                     request.session['admin']=user
                     return HttpResponseRedirect('administrator')
+                elif user[2]=='user' and user[3]==1:
+                    request.session['user']=user
+                    context['user_data']=user
+                    return render(request,'main/index.html',context)
                 else:
                     return render(request,'main/login.html',{'error':"Invalid Credentials"})
             else:
@@ -108,9 +139,75 @@ def viewArtist(request):
         return render(request,'administrator/artistView.html',context)
     return render(request,'error')
 
+def aproveArtist(request):
+    if request.session['admin']:
+        id = request.GET.get('id')
+        sql = "update logintb set status='1' where user_id='"+id+"'"
+        connections.update(sql)
+        sql = "update artist_list set status='1' where artist_id='"+id+"'"
+        connections.update(sql)
+        return HttpResponseRedirect('artistAprovals')
+    return HttpResponseRedirect('error')
 
+def denyArtist(request):
+    if request.session['admin']:
+        id = request.GET.get('id')
+        sql = "update logintb set status='2' where user_id='"+id+"'"
+        connections.update(sql)
+        sql = "update artist_list set status='2' where artist_id='"+id+"'"
+        connections.update(sql)
+        return HttpResponseRedirect('artistAprovals')
+    return HttpResponseRedirect('error')
+
+def editGallery(request):
+    if request.POST:
+        cat_name=request.POST.get('cat')
+        sql = "insert into category_list(name,posts)values('"+cat_name+"','0')"
+        connections.insert(sql)
+    context ={}
+    if request.session['admin']:
+        context['admin']=request.session['admin']
+        sql = "select * from category_list"
+        category_details = connections.selectall(sql)
+        context['category']=category_details
+        return render(request,'administrator/categoryEdit.html',context)
+    return render(request,'error')
+
+
+# Artist
+
+def artistHome(request):
+    context ={}
+    if request.session['artist']:
+        context['artist']=request.session['artist']
+        return render(request,'artist/index.html',context)
+    
+def editArtist(request):
+    context ={}
+    if request.session['artist']:
+        id = request.session['artist'][2]
+        context['artist']=request.session['artist']
+        sql = "select * from artist_list where artist_id='"+id+"'"
+        artist_details = connections.select(sql)
+        context['details']=artist_details
+        return render(request,'artist/editProfile.html',context)
+    return render(request,'error')
+
+def addPost(request):
+    context={}
+    if request.session['artist']:
+        context['artist']=request.session['artist']
+        sql = "select * from category_list"
+        context['cat']=connections.selectall(sql)
+        if request.POST:
+            title = request.POST.get('title')
+        return render(request,'artist/addPost.html',context)
+
+# Error Page
 def error(request):
-    return HttpResponseRedirect('404_error.html')
+    return render(request,'404_error.html')
 
 def test(request):
-    return render(request,'test.html',{'admin':request.session['admin']})
+    sql = "select * from category_list"
+    admin=connections.selectall(sql)
+    return render(request,'test.html',{'admin':admin})
